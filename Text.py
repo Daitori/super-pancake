4d4bec1b293b570cbe780193710b1d0c9dec9a8f
Daitori <100677113+Daitori@users.noreply.github.com>
2022-03-01 10:35:31 +0100

    Update Text.py

--- Text.py
+++ Text.py
@@ -1,1 +1,12 @@
-print("Conjecture de Syracuse")
+print("Conjecture de Syracuse.\n Taper un nombre: ");
+num=int(input());
+t=0;
+while t==0:
+    print(num);
+    if(num%2)==0:
+        num=num/2;
+    else:
+        if num==1:
+            t=1;
+        else:
+            num=(num*3)+1;
